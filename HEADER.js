b4159fd07b5fff1d2b4393eb7227ccba2e6b7a2b
menelike <mitschnacker@gmx.net>
2018-06-24 20:50:36 +0200

    remove server requires to avoid meteor bundling them into client

--- HEADER.js
+++ HEADER.js
@@ -14,18 +14,7 @@ if (typeof document !== 'undefined' && typeof window !== 'undefined') {
   fabric.window = window;
 }
 else {
-  // assume we're running under node.js when document/window are not present
-  fabric.document = require('jsdom')
-    .jsdom(
-      decodeURIComponent('%3C!DOCTYPE%20html%3E%3Chtml%3E%3Chead%3E%3C%2Fhead%3E%3Cbody%3E%3C%2Fbody%3E%3C%2Fhtml%3E'),
-      { features: {
-        FetchExternalResources: ['img']
-      }
-      });
-  fabric.jsdomImplForWrapper = require('jsdom/lib/jsdom/living/generated/utils').implForWrapper;
-  fabric.nodeCanvas = require('jsdom/lib/jsdom/utils').Canvas;
-  fabric.window = fabric.document.defaultView;
-  DOMParser = require('xmldom').DOMParser;
+  throw new Error('this fabric build is for browser usage only');
 }
 
 /**
